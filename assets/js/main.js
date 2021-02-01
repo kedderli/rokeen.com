16268f6f8eec57232fc6abf5c1b0117fdabe3c73
kedderli <gavrish.me@yandex.ru>
2021-02-01 15:54:37 +0300

    Свёрстан хеадер

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+VANTA.DOTS({
+	  el: "#header-animate",
+	  mouseControls: true,
+	  touchControls: true,
+	  gyroControls: false,
+	  minHeight: 200.00,
+	  minWidth: 200.00,
+	  scale: 1.00,
+	  scaleMobile: 1.00,
+	  color: 0xe111d,
+	  color2: 0x3b4256,
+	  backgroundColor: 0xe111d,
+	  size: 0,
+	  spacing: 100.00
+	})
